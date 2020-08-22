322a2857cf4dfaf455f3ed715942527af50dcd4a
Kevin Xu <beaver2025@gmail.com>
2020-08-22 10:46:29 -0400

    Minor upload file changes

--- src/routes/classrooms/uploadFile.js
+++ src/routes/classrooms/uploadFile.js
@@ -1,5 +1,7 @@
 export default async (req, res) => {
 	const files = req.files.files;
+	const classroomId = req.params.classroomId;
+	console.log(classroomId);
 	console.log(files);
 	res.json(files);
 }
