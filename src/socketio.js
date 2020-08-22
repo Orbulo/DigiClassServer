ae1985e2ebd53374a4a5d58fa4aa92a63ea255af
Leon Si <leonzalion@gmail.com>
2020-08-22 01:12:00 -0400

    uncommited node modules

--- src/socketio.js
+++ src/socketio.js
@@ -1,17 +1,18 @@
-import {psubscribeTo} from '~/redis';
+import Redis from 'ioredis';
 
 export default function onConnection(socket) {
+	let currentClassroomId;
+	const sub = new Redis(process.env.REDIS_URL);
+
 	socket.on('disconnect', () => {
 		console.log('User disconnected.');
 	});
 
-	socket.on('connect-to-classroom', (classroomId) => {
-		psubscribeTo(`classroom:${classroomId}:*`, (channel, msg) => {
+	socket.on('connect-to-classroom', async (classroomId) => {
+		await sub.punsubcribe(`classroom:${currentClassroomId}`);
+		currentClassroomId = classroomId;
+		sub.psubscribe(`classroom:${classroomId}:*`, (channel, msg) => {
 			console.log(channel, msg);
 		});
 	});
-
-	socket.on('disconnect-from-classroom', () => {
-
-	})
 }
