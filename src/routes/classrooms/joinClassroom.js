b4b758ff4a87f06bc760f527202fd9fbbb3bd00b
Leon Si <leonzalion@gmail.com>
2020-08-23 02:34:20 -0400

    edits

--- src/routes/classrooms/joinClassroom.js
+++ src/routes/classrooms/joinClassroom.js
@@ -4,12 +4,15 @@ export default async (req, res) => {
 	const userId = req.user.id;
 	const { classroomId } = req.body;
 	if (await redis.exists(`classroom:${classroomId}`)) {
+		if (await redis.sismember(`user:${userId}:classroom-ids`, classroomId)) {
+			throw new Error('Already a member!');
+		}
 		await redis.sadd(`user:${userId}:classroom-ids`, classroomId);
 		await redis.sadd(`classroom:${classroomId}:user-ids`, userId);
 		await redis.hmset(`classroom:${classroomId}:user:${userId}`, {
 			reputation: 0,
 		});
-		res.sendStatus(200);
+		res.json(await redis.hgetall(`classroom:${classroomId}`));
 	} else {
 		throw new Error('Classroom not found.');
 	}
