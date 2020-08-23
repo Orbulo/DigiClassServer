72cf340ca676ca777c8db0813c4352aeb36697ca
Leon Si <leonzalion@gmail.com>
2020-08-22 22:33:23 -0400

    fixed

--- src/socketio.js
+++ src/socketio.js
@@ -1,32 +1,4 @@
 import Redis from 'ioredis';
 import camelcase from 'camelcase';
+import { io } from './bin/www';
 
-export default function onConnection(socket) {
-	let currentClassroomId;
-	const classroomSub = new Redis(process.env.REDIS_URL);
-
-	socket.on('disconnect', () => {
-		console.log('User disconnected.');
-	});
-
-	socket.on('connectToClassroom', async (classroomId) => {
-		socket.join(classroomId);
-		await classroomSub.punsubscribe(`classroom:${currentClassroomId}:*`);
-		currentClassroomId = classroomId;
-		classroomSub.psubscribe(`classroom:${classroomId}:*`);
-		classroomSub.on('pmessage', (pattern, channel, msg) => {
-			console.log(pattern, channel, msg);
-			const topic = channel.split(':').pop();
-			socket.to(classroomId).broadcast.emit(camelcase(topic, { pascalCase: true }), JSON.parse(msg));
-		});
-	});
-
-	socket.on('joinRoom', (roomId, userId) => {
-		socket.join(roomId)
-		socket.to(roomId).broadcast.emit('userConnected', userId)
-
-		socket.on('disconnect', () => {
-			socket.to(roomId).broadcast.emit('userDisconnected', userId)
-		});
-	})
-}
