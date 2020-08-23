a2fd09f7a934b10d701fad3069bcd7a4bc4fd827
Leon Si <leonzalion@gmail.com>
2020-08-22 22:43:17 -0400

    aouestnhasoet

--- src/routes/rooms/getRoom.js
+++ src/routes/rooms/getRoom.js
@@ -2,7 +2,7 @@ import redis from '~/redis';
 
 export default async (req, res) => {
 	const { roomId } = req.params;
-	if (!await redis.exists(roomId)) {
+	if (!await redis.exists(`room:${roomId}`)) {
 		throw new Error('Room not found.');
 	}
 	const roomData = await redis.hgetall(`room:${roomId}`);
